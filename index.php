9a0e86ca478b9a3b8a8fde945ce1603d1d9773e3
pigcake999yt <999pigcake@gmail.com>
2016-10-04 20:38:13 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>PounceLaunce - Launch Your Programs instantly!</title>
+		<meta name="description" content="PounceLaunch is a developer platform to launch projects!">
+		<style>
+		body {
+			margin: 0;
+			padding: 0;
+			font-family: "helvetica neue", helvetica, arial, sans-serif;
+		}
+		.container {
+			width: 100%;
+		}
+		.mainnav {
+			background-image: url("./assets/images");
+			color: white;
+		}
+		.mainnav a {
+			transition: 0.235s;
+			padding: 5px 10px;
+			border-radius: 25px;
+			border: 2px solid white;
+			background: none;
+		}
+		.mainnav a:hover {
+			background-color: rgba(200,200,200,0.3);
+			color: rgba(255,255,255,0.8);
+		}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<!-- Navigation Bar -->
+			<div class="mainnav">
+				<span></span><a class="launch-nav">Launch</a>
+			</div>
+		</div>
+	</body>
+</html>
